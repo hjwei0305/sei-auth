b4ba93b1335cc6ce311525a8de688879300860ce
Vision.Mac <chao2.ma@changhong.com>
2020-04-02 12:30:34 +0800

    增加验证码

--- sei-auth-service/src/main/java/com/changhong/sei/auth/service/LoginHistoryService.java
+++ sei-auth-service/src/main/java/com/changhong/sei/auth/service/LoginHistoryService.java
@@ -7,6 +7,7 @@ import com.changhong.sei.core.cache.CacheBuilder;
 import com.changhong.sei.core.dao.BaseEntityDao;
 import com.changhong.sei.core.dto.ResultData;
 import com.changhong.sei.core.service.BaseEntityService;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -36,6 +37,9 @@ public class LoginHistoryService extends BaseEntityService<LoginHistory> {
      * 记录登录错误次数
      */
     public void recordLoginFailureNum(String tenant, String account) {
+        if (StringUtils.isBlank(tenant)) {
+            tenant = "none";
+        }
         Integer num = cacheBuilder.get(Constants.LOGIN_NUM_KEY + ":" + tenant + ":" + account);
         if (Objects.isNull(num)) {
             num = 0;
@@ -47,12 +51,13 @@ public class LoginHistoryService extends BaseEntityService<LoginHistory> {
      * 检查是否超过登录错误次数
      */
     public ResultData<String> checkLoginFailureNum(String tenant, String account) {
+        if (StringUtils.isBlank(tenant)) {
+            tenant = "none";
+        }
         Integer num = cacheBuilder.get(Constants.LOGIN_NUM_KEY + ":" + tenant + ":" + account);
         if (Objects.isNull(num)) {
             num = 0;
         }
-        cacheBuilder.set(Constants.LOGIN_NUM_KEY + ":" + tenant + ":" + account, ++num, (long) (30 * 60 * 1000));
-
         if (num > 3) {
             return ResultData.fail("登录错误次数超过3次");
         } else {
