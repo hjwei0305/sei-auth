a08a0c9d9f5a14d788978f27ca089e6711b98ed9
Vision.Mac <chao2.ma@changhong.com>
2021-10-26 09:01:03 +0800

    增加oauth2等认证接口

--- sei-auth-api/src/main/java/com/changhong/sei/auth/api/AuthenticationApi.java
+++ sei-auth-api/src/main/java/com/changhong/sei/auth/api/AuthenticationApi.java
@@ -83,46 +83,4 @@ public interface AuthenticationApi {
     Object oauth2(@PathVariable(name = "tenantCode") String tenantCode,
                      @PathVariable(name = "apiPath") String apiPath,
                      HttpServletRequest request, HttpServletResponse response);
-
-    // /**
-    //  * 凭证式
-    //  *
-    //  * @param clientId 应用标示
-    //  * @param secret   应用秘钥
-    //  * @param scope    应用授权作用域
-    //  */
-    // @GetMapping(path = "{tenantCode}/oauth2/clientToken")
-    // @ApiOperation("凭证式")
-    // ResultData<OAuth2Response> clientToken(@PathVariable(name = "tenantCode") String tenantCode,
-    //                                        @RequestParam("clientId") String clientId,
-    //                                        @RequestParam("secret") String secret,
-    //                                        @RequestParam("scope") String scope);
-    //
-    // /**
-    //  * 密码式
-    //  *
-    //  * @param clientId 应用标示
-    //  * @param username 账号
-    //  * @param secret   密码
-    //  */
-    // @PostMapping(path = "{tenantCode}/oauth2/userToken")
-    // @ApiOperation("密码式")
-    // ResultData<OAuth2Response> userToken(@PathVariable(name = "tenantCode") String tenantCode,
-    //                                      @RequestParam("clientId") String clientId,
-    //                                      @RequestParam("secret") String secret,
-    //                                      @RequestParam("username") String username);
-
-    // /**
-    //  * 刷新token
-    //  */
-    // @GetMapping(path = "oauth2/refresh")
-    // @ApiOperation("刷新令牌")
-    // ResultData<Long> refresh();
-
-    // /**
-    //  * 确认授权接口
-    //  */
-    // @GetMapping(path = "oauth2/confirm")
-    // @ApiOperation("确认授权接口")
-    // ResultData<Void> confirm();
 }
